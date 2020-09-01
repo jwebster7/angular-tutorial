0e95d972dcb9d05c7db1b848d7f4ccb640bb7591
Joe Webster <>
2020-09-01 15:31:06 -0500

    setup the base heroes project; created the feature component 'hero-detail'; added messaging and hero services.

---
+++ src/app/hero.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES as Heroes } from './mock-heroes';
+import { Observable, of } from 'rxjs';
+import { MessageService } from './message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroService {
+  constructor(private messageService: MessageService) { }
+  
+  getHeroes(): Observable<Hero[]> {
+    // TODO: send message after getting the heroes.
+    this.messageService.add('HeroService: fetched heroes');
+    return of(Heroes);
+  }
+}
