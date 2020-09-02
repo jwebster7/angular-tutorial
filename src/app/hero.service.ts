e01e2cd9181ee7e14306f58ad0db23aec885cc36
Joe Webster <>
2020-09-02 10:28:28 -0500

    setup in-app navigation with an AppRoutingModule; setup default routes, redirect routes and a parameterized route.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,10 +9,16 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
   constructor(private messageService: MessageService) { }
-  
+
   getHeroes(): Observable<Hero[]> {
     // TODO: send message after getting the heroes.
     this.messageService.add('HeroService: fetched heroes');
     return of(Heroes);
+  }
+
+  getHero(id: number): Observable<Hero> {
+    // TODO: send the message _after_ fetching the hero
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(Heroes.find(hero => hero.id === id));
   }
 }
